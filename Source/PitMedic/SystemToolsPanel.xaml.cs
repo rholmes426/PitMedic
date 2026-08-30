71dd0d057427703d8f0a872c2d3454dc9024d80e
rholmes426 <164943566+rholmes426@users.noreply.github.com>
2026-08-29 20:54:34 -0700

    Add explicit system tools dependencies

--- Source/PitMedic/SystemToolsPanel.xaml.cs
+++ Source/PitMedic/SystemToolsPanel.xaml.cs
@@ -1,6 +1,9 @@
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Threading;
